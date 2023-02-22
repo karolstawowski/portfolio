a756e05a2c437119e85540f62b1e0fbd7360ea95
Karol Stawowski <stawowski.dev@gmail.com>
2023-02-22 19:18:26 +0100

    feat: add xs screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "420px",
+      ...defaultTheme.screens,
+    },
   },
-  plugins: [],
 };
