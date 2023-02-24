be5659d8f914723bf9ea36602822b8dc1798fc76
Karol Stawowski <stawowski.dev@gmail.com>
2023-02-25 00:16:24 +0100

    chore: extend tailwind colors palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,22 @@ module.exports = {
       xs: "420px",
       ...defaultTheme.screens,
     },
+    extend: {
+      colors: {
+        "monitor-inner-border": "#32323C",
+        "monitor-side-pannel": "#0B0D14",
+        "monitor-outer-border": "#393A3C",
+        "monitor-bg": "#131723",
+        "monitor-stand": "#323334",
+        "editor-blue": "#4799EB",
+        "editor-white": "#D9D9D9",
+        "editor-red": "#EB5547",
+        "editor-brown": "#CC9166",
+        "editor-purple": "#DB75F0",
+        "editor-green": "#47EB47",
+        "console-bg": "#441135",
+        "console-menu-bar": "#5F5F5F",
+      },
+    },
   },
 };
