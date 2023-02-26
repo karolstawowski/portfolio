1b518ba7cc1a2a7a5216459f9ed2f5efde2958c4
Karol Stawowski <stawowski.dev@gmail.com>
2023-02-26 22:34:57 +0100

    chore: decrease navigation bar font size

--- src/components/Welcome/NavigationBar.tsx
+++ src/components/Welcome/NavigationBar.tsx
@@ -4,7 +4,7 @@ export const Navbar = (): JSX.Element => {
   return (
     <div className="items-baseline justify-between hidden h-12 px-4 mx-4 my-8 rounded-lg shadow-lg lg:px-8 lg:mx-12 bg-slate-100 md:flex ">
       <h1 className="h-8 my-2 text-2xl select-none">{'<KarolStawowski/>'}</h1>
-      <div className="text-xl">
+      <div className="text-lg">
         {linkTitles.map((title) => (
           <Link key={title} title={title} />
         ))}
