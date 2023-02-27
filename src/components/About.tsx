4fcd992dee5ac5d256e49d8c6a2694dbf93e4538
Karol Stawowski <stawowski.dev@gmail.com>
2023-02-27 13:04:34 +0100

    chore: extract console to separate component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+import { Console } from './About/Console'
 import { SectionTitle } from './SectionTitle'
 
 export const About = (): JSX.Element => {
@@ -16,18 +17,7 @@ export const About = (): JSX.Element => {
           me to make and participate in various projects.
         </p>
       </div>
-      <div className="mt-2 ml-auto rounded-md h-80 xs:h-56 md:h-80 2xl:h-72 bg-console-bg lg:w-11/12 md:ml-0 md:col-span-2">
-        <div className="flex items-center justify-end w-full h-8 gap-1 px-2 rounded-t-md bg-console-menu-bar">
-          <div className="w-4 h-4 rounded-full bg-console-action"></div>
-          <div className="w-4 h-4 rounded-full bg-console-action"></div>
-          <div className="w-4 h-4 rounded-full bg-console-exit"></div>
-        </div>
-        <p className="mx-6 my-4 text-base text-slate-200">
-          Alongside my programming experience, I am pursuing a degree in
-          computer science to acquire knowledge on how to create solutions to
-          real-world problems through technology.
-        </p>
-      </div>
+      <Console />
     </div>
   )
 }
