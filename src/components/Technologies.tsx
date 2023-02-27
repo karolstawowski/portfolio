a53c94dc54615e144cb49f79c13f23466fc08ba5
Karol Stawowski <stawowski.dev@gmail.com>
2023-02-27 12:46:48 +0100

    feat: tighten technologies paragraph

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -9,12 +9,13 @@ export const Technologies = (): JSX.Element => {
   return (
     <div className="py-8 my-8" id="technologies">
       <SectionTitle>Technologies</SectionTitle>
-      <p className="py-4">
-        With my dedication to staying current with the latest frontend
-        technologies, I am eager to apply my skills to exciting projects in the
-        field of mainly - but not only - frontend development.
-      </p>
       <div className="grid md:grid-cols-2 gap-y-6 md:gap-y-0 md:gap-x-8">
+        <p className="py-4">
+          With my dedication to staying current with the latest frontend
+          technologies, I am eager to apply my skills to exciting projects in
+          the field of mainly - but not only - frontend development.
+        </p>
+        <div />
         <div>
           <span className="block mb-3 font-semibold">
             Proficient in technologies:
