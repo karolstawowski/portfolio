e8cfe39b0b8f5e1d3d715ba9a9be57f3ee6039ca
Karol Stawowski <stawowski.dev@gmail.com>
2023-02-28 22:30:32 +0100

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config'
 
-// https://vitejs.dev/config/
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+  },
+}
+
 export default defineConfig({
+  test: vitestConfig.test,
   plugins: [react()],
   base: './',
 })
