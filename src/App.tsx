ba035ddf495126d80b1b3ccce65e0f2cab7d7ab3
Karol Stawowski <stawowski.dev@gmail.com>
2023-09-14 18:54:40 +0200

    chore: move navbar to welcome

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,10 @@ import { Footer } from './components/Footer'
 import { Projects } from './components/Projects'
 import { Technologies } from './components/Technologies'
 import { Welcome } from './components/Welcome'
-import { Navbar } from './components/Welcome/NavigationBar'
 
 const App = (): JSX.Element => {
   return (
     <div className="flex flex-col items-center bg-background text-text">
-      <Navbar />
       <Welcome />
       <About />
       <Technologies />
