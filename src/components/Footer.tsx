f5520d07fcbc4c8cbac91524e6b7a85243890db0
Karol Stawowski <stawowski.dev@gmail.com>
2026-02-01 12:40:07 +0100

    chore: update footer with 'to' year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { JSX } from 'react'
 export const Footer = (): JSX.Element => {
   return (
     <div className="flex items-center justify-center w-full h-24 bg-background text-text-secondary xs:w-10/12 md:w-3/4 xl:w-9/12 2xl:w-8/12 max-w-8xl">
-      <h4>Copyright © 2024 Karol Stawowski</h4>
+      <h4>Copyright © 2024 - {new Date().getFullYear()} Karol Stawowski</h4>
     </div>
   )
 }
